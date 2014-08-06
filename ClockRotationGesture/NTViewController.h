c21832471cca110ca060d3346b5cd12b6f7398c1
ビザンコムマック０９ <bizan09@bizankomumakku09-no-MacBook-Air.local>
2014-08-06 13:48:08 +0900

    Initial Commit

---
+++ ClockRotationGesture/NTViewController.h
@@ -0,0 +1,13 @@
+//
+//  NTViewController.h
+//  ClockRotationGesture
+//
+//  Created by ビザンコムマック０９ on 2014/08/06.
+//  Copyright (c) 2014年 ビザンコムマック０９. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTViewController : UIViewController
+
+@end
