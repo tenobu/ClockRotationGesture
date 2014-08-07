4a154726909e8f4731fa096c6f902ab6ac0d3c41
tenobu <bizan09@bizankomumakku09-no-MacBook-Air.local>
2014-08-07 14:28:30 +0900

    NTClockSettingViewの入れ物など

---
+++ ClockRotationGesture/NTClockSettingView.h
@@ -0,0 +1,13 @@
+//
+//  NTClockSettingView.h
+//  ClockRotationGesture
+//
+//  Created by ビザンコムマック０９ on 2014/08/07.
+//  Copyright (c) 2014年 ビザンコムマック０９. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTClockSettingView : UIView
+
+@end
---
+++ ClockRotationGesture/NTClockSettingView.m
@@ -0,0 +1,31 @@
+//
+//  NTClockSettingView.m
+//  ClockRotationGesture
+//
+//  Created by ビザンコムマック０９ on 2014/08/07.
+//  Copyright (c) 2014年 ビザンコムマック０９. All rights reserved.
+//
+
+#import "NTClockSettingView.h"
+
+@implementation NTClockSettingView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
