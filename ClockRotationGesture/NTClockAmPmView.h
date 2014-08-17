4c4acb3e7e642d823628b0523a357ab8de14e0fc
tenobu <bizan09@bizankomumakku09-no-MacBook-Air.local>
2014-08-17 15:48:43 +0900

    NTClockAmPmView の初期化

---
+++ ClockRotationGesture/NTClockAmPmView.h
@@ -0,0 +1,13 @@
+//
+//  NTClockAmPmView.h
+//  ClockRotationGesture
+//
+//  Created by ビザンコムマック０９ on 2014/08/17.
+//  Copyright (c) 2014年 ビザンコムマック０９. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTClockAmPmView : UIView
+
+@end
---
+++ ClockRotationGesture/NTClockAmPmView.m
@@ -0,0 +1,31 @@
+//
+//  NTClockAmPmView.m
+//  ClockRotationGesture
+//
+//  Created by ビザンコムマック０９ on 2014/08/17.
+//  Copyright (c) 2014年 ビザンコムマック０９. All rights reserved.
+//
+
+#import "NTClockAmPmView.h"
+
+@implementation NTClockAmPmView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
